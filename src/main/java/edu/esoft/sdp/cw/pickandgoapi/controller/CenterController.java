9538e83d2421b746df46e2aef65f686750dc1391
Thisal <dinukathisal.1995@gmail.com>
2022-02-09 22:32:50 +0530

    [SDPCW-18] - changed center path.

--- src/main/java/edu/esoft/sdp/cw/pickandgoapi/controller/CenterController.java
+++ src/main/java/edu/esoft/sdp/cw/pickandgoapi/controller/CenterController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @CrossOrigin
-@RequestMapping(value = "/api/v1/centers")
+@RequestMapping(value = "/centers")
 @RequiredArgsConstructor
 public class CenterController {
 
