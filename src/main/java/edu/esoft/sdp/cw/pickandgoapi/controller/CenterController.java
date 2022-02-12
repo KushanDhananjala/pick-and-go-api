024e1ea1d8803acb96d382d3961d6986bd62995e
Thisal <dinukathisal.1995@gmail.com>
2022-02-12 12:02:03 +0530

    [SDPCW-21] - Implemented one API to get ACTIVE and INACTIVE centers.

--- src/main/java/edu/esoft/sdp/cw/pickandgoapi/controller/CenterController.java
+++ src/main/java/edu/esoft/sdp/cw/pickandgoapi/controller/CenterController.java
@@ -1,13 +1,21 @@
 package edu.esoft.sdp.cw.pickandgoapi.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import edu.esoft.sdp.cw.pickandgoapi.dto.CenterDTO;
 import edu.esoft.sdp.cw.pickandgoapi.service.CenterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -28,20 +36,13 @@ public class CenterController {
     }
   }
 
-  @GetMapping(value = "/all-active")
-  public ResponseEntity<List<CenterDTO>> getAllActiveCenters() {
+  @GetMapping(value = "/{status}")
+  public ResponseEntity<List<CenterDTO>> getAllActiveCenters(@PathVariable String status) {
     try {
-      return ResponseEntity.ok(centerService.getAllActiveCenters());
-    } catch (Exception e) {
-      log.error(e.getMessage());
-      return null;
-    }
-  }
-
-  @GetMapping(value = "/all-inactive")
-  public ResponseEntity<List<CenterDTO>> getAllInActiveCenters() {
-    try {
-      return ResponseEntity.ok(centerService.getAllInActiveCenters());
+      return ResponseEntity.ok(
+          "ACTIVE".equalsIgnoreCase(status)
+              ? centerService.getAllActiveCenters()
+              : centerService.getAllInActiveCenters());
     } catch (Exception e) {
       log.error(e.getMessage());
       return null;
